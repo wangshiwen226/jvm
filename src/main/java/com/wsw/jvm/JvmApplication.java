435d72a0c2eaf8470046925cb3438aaf8dd971a3
wsw <wswmail@yeah.net>
2021-03-31 17:17:13 +0800

    Initial commit

---
+++ src/main/java/com/wsw/jvm/JvmApplication.java
@@ -0,0 +1,16 @@
+package com.wsw.jvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wsw
+ */
+@SpringBootApplication
+public class JvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvmApplication.class, args);
+    }
+
+}
